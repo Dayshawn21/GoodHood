8036339497a39259bde8d04b97397acd3c5e72ca
dayshawn terry <dayshawn21@gmail.com>
2021-06-30 20:21:43 -0500

    serverside

--- pages/Episodes.js
+++ pages/Episodes.js
@@ -1,31 +1,33 @@
-import { API_URL } from '../utils/urls';
-import EspCard from '../componets/EspCard';
-import podcastStyle from '../styles/Podcast.module.scss';
+import { API_URL } from "../utils/urls";
+import EspCard from "../componets/EspCard";
+import podcastStyle from "../styles/Podcast.module.scss";
 const Episodes = ({ podcasts }) => {
-	return (
-		<div className='container'>
-			<div className={podcastStyle.podcast}>
-				<h1 className='text-center py-4'> Episodes </h1>
-				<div className={podcastStyle.grid}>
-					{podcasts && podcasts.map((podcast) => <EspCard podcast={podcast} />)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className={podcastStyle.podcast}>
+        <h1 className="text-center py-4"> Episodes </h1>
+        <div className={podcastStyle.grid}>
+          {podcasts.map((podcast) => (
+            <EspCard podcast={podcast} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 export const getStaticProps = async () => {
-	// Fetch Products
+  // Fetch Products
 
-	const podcast_res = await fetch(`${API_URL}/podcasts`);
-	const podcasts = await podcast_res.json();
+  const podcast_res = await fetch(`${API_URL}/podcasts`);
+  const podcasts = await podcast_res.json();
 
-	// return Products
+  // return Products
 
-	return {
-		props: {
-			podcasts,
-		},
-	};
+  return {
+    props: {
+      podcasts,
+    },
+  };
 };
 
 export default Episodes;
